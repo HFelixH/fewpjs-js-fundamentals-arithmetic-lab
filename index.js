160e133c31b3886b6cd1db96f9737fa946344a9f
HFelixH <felix-hoang@outlook.com>
2022-07-14 18:34:02 +0930

    Upload Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2;
+
+console.log (multiply)
+//===========
+
+const random=Math.floor(Math.random() * 10 +1)
+
+console.log (random)
+//===========
+
+const num3 = 9
+const num4 = 5
+
+const mod =num3 % num4
+
+console.log (mod)
+//===========
+
+const max=Math.max(0, 5, 10, 15, 20)
+
+console.log (max)
